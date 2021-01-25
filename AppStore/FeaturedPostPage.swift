b485c5390453972c10ec84af42774b7f90f01018
Zeeshan A Zakaria <it@zeeshanz.com>
2021-01-24 20:02:06 -0500

    Set Profile image to render as template image, so that it displays in default blue. Removed an unused image.

--- AppStore/FeaturedPostPage.swift
+++ AppStore/FeaturedPostPage.swift
@@ -19,12 +19,13 @@ struct FeaturedPostView : View {
             Text(titleText).font(.title3).bold()
             Text(subTitleText).font(.subheadline).foregroundColor(.gray)
             Image(image).resizable().frame(width: (UIApplication.shared.windows.first?.frame.width ?? 0) - 50, height: 230, alignment: .center).cornerRadius(5.0)
-        }.frame(minWidth: 0, maxWidth: .infinity, alignment: .leading).padding(10)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading).padding(10)
     }
 }
 
 struct FeaturedPost_Previews: PreviewProvider {
     static var previews: some View {
-        FeaturedPostView(captionText: "New Update", titleText: "The Sims Freeplay", subTitleText: "Bonjour from Paris", image: "sim-city")
+        FeaturedPostView(captionText: "New Update", titleText: "The Sims Freeplay", subTitleText: "Bonjour from Paris", image: "sample-app-1")
     }
 }
